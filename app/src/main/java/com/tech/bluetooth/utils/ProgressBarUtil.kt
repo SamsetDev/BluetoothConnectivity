2f7182508e1614c5ab7774a9d5c35f2f8ea0e807
Sanjay Singh <a_sanjay.singh@airtelbank.com>
2023-12-11 21:02:20 +0530

    Handle singlton class with thread safe

--- app/src/main/java/com/tech/bluetooth/utils/ProgressBarUtil.kt
+++ app/src/main/java/com/tech/bluetooth/utils/ProgressBarUtil.kt
@@ -1,39 +1,108 @@
 package com.tech.bluetooth.utils
 
+import android.app.Activity
+import android.app.Dialog
 import android.content.Context
+import android.graphics.drawable.ColorDrawable
 import android.view.Gravity
+import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.FrameLayout
 import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.view.ContextThemeWrapper
+import com.tech.bluetooth.R
 
 object ProgressBarUtil {
-    private var progressBar: ProgressBar? = null
 
-    fun showProgressBar(context: Context) {
-        if (progressBar == null) {
-            progressBar = createProgressBar(context)
+    private var alertDialog: Dialog? = null
+
+
+    /***
+     * To dismiss the dialog
+     */
+    fun dismissLoadingDialog() {
+        try {
+            if (alertDialog != null && alertDialog!!.isShowing) {
+                alertDialog?.dismiss()
+                alertDialog?.cancel()
+            }
+        } catch (e: IllegalArgumentException) {
+           e.printStackTrace()
+        } finally {
+            alertDialog = null
         }
-
-        progressBar?.visibility = ProgressBar.VISIBLE
     }
 
-    fun hideProgressBar() {
-        progressBar?.visibility = ProgressBar.GONE
+    fun showLoadingDialog(context: Context,type:PROGRESSBAR_TYPE) {
+        val themeSmall = ContextThemeWrapper(context, android.R.style.Widget_ProgressBar_Small)
+        val themeLarge = ContextThemeWrapper(context, android.R.style.Widget_ProgressBar_Large)
+
+        try {
+            if (context is Activity) {
+                val activity: Activity = context
+                if (activity.isFinishing) {
+                    return
+                }
+            }
+            if (alertDialog != null && alertDialog!!.isShowing) {
+                return
+            }
+            val result = if (type == PROGRESSBAR_TYPE.LARGE) themeLarge else themeSmall
+            alertDialog = Dialog(result)
+            alertDialog.let {
+                it?.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                it?.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                it?.setContentView(R.layout.progress_view)
+                (it?.findViewById(R.id.progress_bar) as ProgressBar).visibility = View.VISIBLE
+                it?.setCanceledOnTouchOutside(false)
+                it?.setCancelable(false)
+                it?.window?.setGravity(Gravity.CENTER)
+                it?.window?.setBackgroundDrawable(ColorDrawable(0))
+                it?.show()
+            }
+
+
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+        }
     }
 
-    private fun createProgressBar(context: Context): ProgressBar {
-        val progressBar = ProgressBar(context)
-        progressBar.visibility = ProgressBar.GONE
+    fun showLoadingDialogWithMessage(context: Context,msg:String,type:PROGRESSBAR_TYPE) {
+        val themeSmall = ContextThemeWrapper(context, android.R.style.Widget_ProgressBar_Small)
+        val themeLarge = ContextThemeWrapper(context, android.R.style.Widget_ProgressBar_Large)
 
-        val frameLayout = FrameLayout(context)
-        val layoutParams = FrameLayout.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        layoutParams.gravity = Gravity.CENTER
-        frameLayout.layoutParams = layoutParams
-        frameLayout.addView(progressBar)
+        try {
+            if (context is Activity) {
+                val activity: Activity = context
+                if (activity.isFinishing) {
+                    return
+                }
+            }
+            if (alertDialog != null && alertDialog!!.isShowing) {
+                return
+            }
 
-        return progressBar
+            val result = if (type == PROGRESSBAR_TYPE.LARGE) themeLarge else themeSmall
+            alertDialog = Dialog(result)
+            alertDialog?.let {
+                it.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                it.setContentView(R.layout.progress_view)
+                (it.findViewById(R.id.progress_bar) as ProgressBar).visibility = View.VISIBLE
+                val progressMsg = it.findViewById(R.id.progressbarMsg) as TextView
+                progressMsg.text = msg
+                it.setCanceledOnTouchOutside(false)
+                it.setCancelable(false)
+                it.window?.setBackgroundDrawable(ColorDrawable(0))
+                it.show()
+            }
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+        }
     }
+    enum class PROGRESSBAR_TYPE{
+        LARGE,SMALL
+    }
+
 }
